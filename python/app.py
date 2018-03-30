52e153e5a9bd1dc574d88a4ada0e3be1c090343b
Brandon Cooke <brandoncookedev@gmail.com>
2018-03-29 22:45:58 -0400

    Crop GO! text of melee screen and get edges. Should be able to use this to determine when GO is on screen for a video

--- python/app.py
+++ python/app.py
@@ -7,12 +7,27 @@ RESOURCES_DIR = os.path.join('..', 'resources')
 melee_img = os.path.join(RESOURCES_DIR, 'meleeGO.jpg')
 smash4_img = os.path.join(RESOURCES_DIR, 'smash4GO.jpg')
 
+#read image into cv2 and crop area with GO! text
 img = cv2.imread(melee_img, 0)
-edges = cv2.Canny(img, 200, 500)
+cropped_img = img[200:800, 450:1550]
 
+# optional show cropped image
+#cv2.imshow("lul", cropped_img)
+#cv2.waitKey(0)
+
+#use canny algo to get edges of an image with a low and high threshold
+edges = cv2.Canny(cropped_img, 250, 550)
+
+#optional show original vs cropped image
+"""
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+"""
 
-plt.show()
+cv2.imshow("helloworldlul", edges)
+cv2.waitKey(0)
+cv2.imwrite('meleeGOedges.jpg', edges)
+
+#plt.show()
