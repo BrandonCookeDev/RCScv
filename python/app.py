9b31df3801a1fd660bfc9a98eece121b79698275
Brandon Cooke <brandoncookedev@gmail.com>
2018-04-25 21:19:14 -0400

    added main resource logic and new melee frames

--- python/app.py
+++ python/app.py
@@ -6,3 +6,10 @@ from lib import RCScv as rcscv
 logger = logging.getLogger('RCScv')
 logger.info('RCScv: Beginning Main')
 
+ROOT_DIR = os.path.dirname(__file__)
+RESOURCES_DIR = os.path.join(ROOT_DIR, '..', 'resources')
+MELEE1 = os.path.join(RESOURCES_DIR, 'Melee1.png')
+MELEE2 = os.path.join(RESOURCES_DIR, 'Melee2.png')
+
+if __name__ == '__main__':
+    pass
