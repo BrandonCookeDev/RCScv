77ab9c27aafae96f72849031fdd0db710caf0939
Jarrod Blanton <jarrodblanton13@gmail.com>
2018-04-28 17:38:11 -0400

    stuff

--- python/lib/percent_cropper.py
+++ python/lib/percent_cropper.py
@@ -4,7 +4,12 @@ import logging
 from lib.RCScv import RCScv as RCScv
 from util.config import Config as Config
 
+config = Config()
+logger = logging.getLogger('RCScv')
+
 def process_frame(frame): 
     print('we out here fam')
+    default low = config.
     p1 = RCScv(frame, 'p1frame.png')
+
     p1.show()
