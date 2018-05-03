7f874bff386697ba776aabebbcf6159722330f80
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-05-03 00:00:45 -0400

    Added FrameBuffer object to the Models.

--- python/lib/Models.py
+++ python/lib/Models.py
@@ -8,3 +8,108 @@ class Match(object):
     def __init__(self, round, best_of)
         self.round = round
         self.best_of = best_of
+
+class FrameBuffer(object):
+    def __init__(self, buffer_size):
+        self.buffer_size = buffer_size
+
+    
+    def add(self, key, val):
+        """
+        FrameBuffer add will add an object property to an object and set it to 
+        an array with the value specified in it. If the array at the property name
+        exists and is over the length specified by self.buffer_size, element 0 will
+        be popped from the array at the property.
+
+        example: 
+        self.buffer_size = 7
+        {
+            "P1S1": [55, 66, 44, 78, 65, 43, 23]
+            "P1S2": [33, 66, 22, 11, 23, 43, 14]
+            "P1S3": [55, 66, 44, 78, 23]
+        }
+        add("PS1S", 67)
+        add("PS3S", 69)
+        yields
+        {
+            "P1S1": [66, 44, 78, 65, 43, 23, 67] #55 removed, queue shifts left, 67 is added
+            "P1S2": [33, 66, 22, 11, 23, 43, 14]
+            "P1S3": [55, 66, 44, 78, 23, 69]     #69 added because len < self.buffer_size
+        }
+
+            :param self: this object
+            :param key: name for the object property this will be stored at
+            :param val: value to be stored in the array at `name`
+        """   
+
+        assert key is not None, 'FrameBuffer.add: Key may not be None'
+        assert val is not None, 'FrameBuffer.add: Val may not be None'
+
+        if key not in self:
+            self[key] = []
+        elif len(self[key]) > buffer_size:
+            del self[key][0]
+        self[key].append(val)
+
+    def pop(self, key):
+        """
+        FrameBuffer pop removes the first element from the array at the specified
+        property name, and returns that value to the caller
+            :param self: this object
+            :param key: name of the property owning the array to pop first element
+        """   
+
+        assert key is not None, 'FrameBuffer.pop key cannot be None'
+        assert key is in self, 'FrameBuffer.pop key must be a property of FrameBuffer object'
+
+        val = self[key][0]
+        del self[key][0]
+        return val
+
+    def get(self, key, index):
+        """
+        FrameBuffer get retrieves the element at the specified property name and array index
+        and returns it to the caller
+            :param self: this object
+            :param key: property name owning the target array
+            :param index: array index of the desired element
+        """   
+
+        assert key is not None, 'FrameBuffer.get key cannot be None'
+        assert key is in self, 'FrameBuffer.get key must be a property of FrameBuffer object'
+        assert index is not None, 'FrameBuffer.get index cannot be None'
+        assert len(self[key] > index), 'FrameBuffer.get index is out of range for the array at key %s' % key
+
+        val = self[key][index]
+        return val
+
+    def remove(self, key, index):
+        """
+        FrameBuffer remove deletes a value from the array owned by the given property name
+        at the given index
+            :param self: this object
+            :param key: property name owning the target array
+            :param index: array index of the desired element
+        """
+
+        assert key is not None, 'FrameBuffer.get key cannot be None'
+        assert key is in self, 'FrameBuffer.get key must be a property of FrameBuffer object'
+        assert index is not None, 'FrameBuffer.get index cannot be None'
+        assert len(self[key] > index), 'FrameBuffer.get index is out of range for the array at key %s' % key
+
+        del self[key][index]
+
+    def average_stock_image_buffers(self, key):
+        """
+        FrameBuffer average takes a property name, and returns the average of
+        the values in the array located at that property name 
+            :param self: this object
+            :param key: name of the property owning the array to be averaged
+        """   
+        assert self[key] is not None, 'Null image buffer for key %s' % key
+
+        sum = 0
+        count = len(self[key])
+        for element in self[key]:
+            sum += element
+        return sum/count
