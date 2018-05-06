298d79b03172c11b52eb97a4bba09d04031166db
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-05-06 01:19:06 -0400

    Added threader class. not tested

---
+++ python/lib/Threder.py
@@ -0,0 +1,36 @@
+import os, sys
+import threading
+
+import logging
+logger = logging.getLogger('RCScv')
+
+class Worker(object):
+
+    def __init__(self, name, limit=0, buffer=[]):
+        self.name = name
+        self.limit = limit
+        self.buffer = buffer
+
+    def add(fn):
+        assert fn is not None, 'Thread must have a function to execute'
+        assert callable(fn), 'fn parameter must be a function'
+
+        logger.info('Adding Thread %s' % self.name)
+
+        t = threading.Thread(target=fn)
+        self.buffer.append(t)
+
+    def run():
+        logger.info('Beginning thread buffer %s' % self.name)
+        active = 0
+        while len(self.buffer) > 0:
+            while active >= self.limit:
+                pass #wait until barrier is reached
+            
+            t = buffer.pop(0)
+            t.start()
+
+            logger.debug('Began thread for %s. [Active: %s :: Limit: %s]' % 
+                (self.name, active, self.limit))
+
+
